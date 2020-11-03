a100462d64345faf029886075e3dbea05378646a
Iván (profe) <ivan@adalab.es>
2020-11-03 10:59:54 +0100

    Add a minimal component

---
+++ modulo-3-leccion-02/my-form/src/ComponenteAlicia.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import "./ComponenteAlicia.css";
+
+class ComponenteAlicia extends Component {
+
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    
+    return (
+      <div className="Alicia"></div>
+    );
+  }
+}
+
+export default ComponenteAlicia;
