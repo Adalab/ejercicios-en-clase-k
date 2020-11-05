b7d7050449bb88ed2dd639f90360debf1bd0796c
Iván (profe) <ivan@adalab.es>
2020-11-05 12:05:41 +0100

    Upload my-template (this is the last time)

---
+++ modulo-3-leccion-04/my-template/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <h1 className="header__title">3.4: Xxx</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
