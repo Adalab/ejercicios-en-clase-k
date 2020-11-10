d306c3298790bc7c8b76be600baa1a8d8f414c5d
VanGamo <vangamo.beta@gmail.com>
2020-11-10 12:40:18 +0100

    Change class to className in Zaralab

--- modulo-3-leccion-06/zaralab-template/src/components/App.js
+++ modulo-3-leccion-06/zaralab-template/src/components/App.js
@@ -9,18 +9,18 @@ class App extends React.Component {
         <Header number="2.6" lesson="Estado" title="Zaralab" />
         <main className="container">
           <h2>Zaralab</h2>
-          <div class="store">
-            <section class="shelves">
-              <h3 class="shelves__title">Lista de productos</h3>
-              <article class="shelves__article">
-                <div class="shelves__article--imgContainer">
-                  <img class="shelves__article--img" src="./images/tshirt-simple-by_vexels.png" />
+          <div className="store">
+            <section className="shelves">
+              <h3 className="shelves__title">Lista de productos</h3>
+              <article className="shelves__article">
+                <div className="shelves__article--imgContainer">
+                  <img className="shelves__article--img" src="./images/tshirt-simple-by_vexels.png" />
                 </div>
-                <h3 class="shelves__article--title">Camiseta</h3>
-                <div class="shelves__article--buyPanel">
-                  <div class="buyPanel__size">
-                    <h4 class="buyPanel__section--title">Talla:</h4>
-                    <select name="" id="" class="buyPanel__size--select">
+                <h3 className="shelves__article--title">Camiseta</h3>
+                <div className="shelves__article--buyPanel">
+                  <div className="buyPanel__size">
+                    <h4 className="buyPanel__section--title">Talla:</h4>
+                    <select name="" id="" className="buyPanel__size--select">
                       <option value="" disabled selected>
                         Escoge:
                       </option>
@@ -31,28 +31,28 @@ class App extends React.Component {
                       <option value="XL">XL</option>
                     </select>
                   </div>
-                  <div class="buyPanel__amount">
-                    <h4 class="buyPanel__section--title">Cantidad:</h4>
-                    <button class="buyPanel__amount--add">+</button>
-                    <input type="text" name="" id="" class="buyPanel__amount--number" value="2" />
-                    <button class="buyPanel__amount--sub">-</button>
+                  <div className="buyPanel__amount">
+                    <h4 className="buyPanel__section--title">Cantidad:</h4>
+                    <button className="buyPanel__amount--add">+</button>
+                    <input type="text" name="" id="" className="buyPanel__amount--number" value="2" />
+                    <button className="buyPanel__amount--sub">-</button>
                   </div>
-                  <div class="buyPanel__price">
-                    <h4 class="buyPanel__section--title">Precio:</h4>
-                    <span class="buyPanel__price--number">39</span>€
+                  <div className="buyPanel__price">
+                    <h4 className="buyPanel__section--title">Precio:</h4>
+                    <span className="buyPanel__price--number">39</span>€
                   </div>
-                  <button class="buyPanel__buy">Comprar</button>
+                  <button className="buyPanel__buy">Comprar</button>
                 </div>
               </article>
-              <article class="shelves__article">
-                <div class="shelves__article--imgContainer">
-                  <img class="shelves__article--img" src="./images/pants-simple-by_vexels.png" />
+              <article className="shelves__article">
+                <div className="shelves__article--imgContainer">
+                  <img className="shelves__article--img" src="./images/pants-simple-by_vexels.png" />
                 </div>
-                <h3 class="shelves__article--title">Pantalón</h3>
-                <div class="shelves__article--buyPanel">
-                  <div class="buyPanel__size">
-                    <h4 class="buyPanel__section--title">Talla:</h4>
-                    <select name="" id="" class="buyPanel__size--select">
+                <h3 className="shelves__article--title">Pantalón</h3>
+                <div className="shelves__article--buyPanel">
+                  <div className="buyPanel__size">
+                    <h4 className="buyPanel__section--title">Talla:</h4>
+                    <select name="" id="" className="buyPanel__size--select">
                       <option value="" disabled selected>
                         Escoge:
                       </option>
@@ -63,28 +63,28 @@ class App extends React.Component {
                       <option value="XL">XL</option>
                     </select>
                   </div>
-                  <div class="buyPanel__amount">
-                    <h4 class="buyPanel__section--title">Cantidad:</h4>
-                    <button class="buyPanel__amount--add">+</button>
-                    <input type="text" name="" id="" class="buyPanel__amount--number" value="2" />
-                    <button class="buyPanel__amount--sub">-</button>
+                  <div className="buyPanel__amount">
+                    <h4 className="buyPanel__section--title">Cantidad:</h4>
+                    <button className="buyPanel__amount--add">+</button>
+                    <input type="text" name="" id="" className="buyPanel__amount--number" value="2" />
+                    <button className="buyPanel__amount--sub">-</button>
                   </div>
-                  <div class="buyPanel__price">
-                    <h4 class="buyPanel__section--title">Precio:</h4>
-                    <span class="buyPanel__price--number">59</span>€
+                  <div className="buyPanel__price">
+                    <h4 className="buyPanel__section--title">Precio:</h4>
+                    <span className="buyPanel__price--number">59</span>€
                   </div>
-                  <button class="buyPanel__buy">Comprar</button>
+                  <button className="buyPanel__buy">Comprar</button>
                 </div>
               </article>
-              <article class="shelves__article">
-                <div class="shelves__article--imgContainer">
-                  <img class="shelves__article--img" src="./images/arctic-boots-illustration-by-Vexels.png" />
+              <article className="shelves__article">
+                <div className="shelves__article--imgContainer">
+                  <img className="shelves__article--img" src="./images/arctic-boots-illustration-by-Vexels.png" />
                 </div>
-                <h3 class="shelves__article--title">Botas</h3>
-                <div class="shelves__article--buyPanel">
-                  <div class="buyPanel__size">
-                    <h4 class="buyPanel__section--title">Talla:</h4>
-                    <select name="" id="" class="buyPanel__size--select">
+                <h3 className="shelves__article--title">Botas</h3>
+                <div className="shelves__article--buyPanel">
+                  <div className="buyPanel__size">
+                    <h4 className="buyPanel__section--title">Talla:</h4>
+                    <select name="" id="" className="buyPanel__size--select">
                       <option value="" disabled selected>
                         Escoge:
                       </option>
@@ -95,28 +95,28 @@ class App extends React.Component {
                       <option value="XL">XL</option>
                     </select>
                   </div>
-                  <div class="buyPanel__amount">
-                    <h4 class="buyPanel__section--title">Cantidad:</h4>
-                    <button class="buyPanel__amount--add">+</button>
-                    <input type="text" name="" id="" class="buyPanel__amount--number" value="2" />
-                    <button class="buyPanel__amount--sub">-</button>
+                  <div className="buyPanel__amount">
+                    <h4 className="buyPanel__section--title">Cantidad:</h4>
+                    <button className="buyPanel__amount--add">+</button>
+                    <input type="text" name="" id="" className="buyPanel__amount--number" value="2" />
+                    <button className="buyPanel__amount--sub">-</button>
                   </div>
-                  <div class="buyPanel__price">
-                    <h4 class="buyPanel__section--title">Precio:</h4>
-                    <span class="buyPanel__price--number">89</span>€
+                  <div className="buyPanel__price">
+                    <h4 className="buyPanel__section--title">Precio:</h4>
+                    <span className="buyPanel__price--number">89</span>€
                   </div>
-                  <button class="buyPanel__buy">Comprar</button>
+                  <button className="buyPanel__buy">Comprar</button>
                 </div>
               </article>
-              <article class="shelves__article">
-                <div class="shelves__article--imgContainer">
-                  <img class="shelves__article--img" src="./images/winter-blankets-flat-by-Vexels.png" />
+              <article className="shelves__article">
+                <div className="shelves__article--imgContainer">
+                  <img className="shelves__article--img" src="./images/winter-blankets-flat-by-Vexels.png" />
                 </div>
-                <h3 class="shelves__article--title">Mantitas suaves</h3>
-                <div class="shelves__article--buyPanel">
-                  <div class="buyPanel__size">
-                    <h4 class="buyPanel__section--title">Talla:</h4>
-                    <select name="" id="" class="buyPanel__size--select">
+                <h3 className="shelves__article--title">Mantitas suaves</h3>
+                <div className="shelves__article--buyPanel">
+                  <div className="buyPanel__size">
+                    <h4 className="buyPanel__section--title">Talla:</h4>
+                    <select name="" id="" className="buyPanel__size--select">
                       <option value="" disabled selected>
                         Escoge:
                       </option>
@@ -127,27 +127,27 @@ class App extends React.Component {
                       <option value="XL">XL</option>
                     </select>
                   </div>
-                  <div class="buyPanel__amount">
-                    <h4 class="buyPanel__section--title">Cantidad:</h4>
-                    <button class="buyPanel__amount--add">+</button>
-                    <input type="text" name="" id="" class="buyPanel__amount--number" value="2" />
-                    <button class="buyPanel__amount--sub">-</button>
+                  <div className="buyPanel__amount">
+                    <h4 className="buyPanel__section--title">Cantidad:</h4>
+                    <button className="buyPanel__amount--add">+</button>
+                    <input type="text" name="" id="" className="buyPanel__amount--number" value="2" />
+                    <button className="buyPanel__amount--sub">-</button>
                   </div>
-                  <div class="buyPanel__price">
-                    <h4 class="buyPanel__section--title">Precio:</h4>
-                    <span class="buyPanel__price--number">14</span>€
+                  <div className="buyPanel__price">
+                    <h4 className="buyPanel__section--title">Precio:</h4>
+                    <span className="buyPanel__price--number">14</span>€
                   </div>
-                  <button class="buyPanel__buy">Comprar</button>
+                  <button className="buyPanel__buy">Comprar</button>
                 </div>
               </article>
             </section>
-            <section class="cart">
-              <h3 class="cart__title">Cesta de la compra</h3>
-              <ul class="cart__list">
-                <li class="cart__list--item">
-                  <span class="cartListItem__concept">Camiseta</span>
-                  <span class="cartListItem__amount">x 1</span>
-                  <span class="cartListItem__price">69€</span>
+            <section className="cart">
+              <h3 className="cart__title">Cesta de la compra</h3>
+              <ul className="cart__list">
+                <li className="cart__list--item">
+                  <span className="cartListItem__concept">Camiseta</span>
+                  <span className="cartListItem__amount">x 1</span>
+                  <span className="cartListItem__price">69€</span>
                 </li>
               </ul>
             </section>
